ab52fe5467c5dea5268203920c24c25b349815f4
Mark Thanadabouth <mark28ten@gmail.com>
2021-08-04 21:52:24 -0700

    halfway done with lab08

--- server.js
+++ server.js
@@ -1,56 +1,63 @@
 'use-strict';
 
-require('dotenv').config();
-
-const PORT = process.env.PORT
-
+// in our servers, we must use require instead of import
 const express = require('express');
-const { response } = require('express');
-
+// use dotenv to access our .env file -- must be done BEFORE defining PORT
+require('dotenv').config();
+// how its as per the docs
 const app = express();
-
-let weatherData = require('./data/weather.json');
-
-class Forecast {
-  constructor(description, date) {
-    this.description = description;
-    this.date = date;
-  }
-}
-
 // allow frontend to access data from backend
 const cors = require('cors');
 app.use(cors());
 
-// app.get('/weather', (request, response) => {
-//   response.send(weatherData);
-// });
+const axios = require('axios');
+const PORT = process.env.PORT
 
-app.get('/weather', (request, response) => {
-  let searchQuery = request.query.city;
-  // console.log(searchQuery);
-  let weatherArr = [];
-  if (searchQuery) {
-    let cityWeather = weatherData.find((city) => city.city_name === searchQuery);
-    // console.log(cityWeather);
-    if (cityWeather) {
-      cityWeather.data.map((weather) => {
-        weatherArr.push(new Forecast(`Forecast for ${weather.datetime}: Low: ${weather.low_temp}, High: ${weather.high_temp} with ${weather.weather.description}`, weather.datetime)
-        );
-      });
-      response.send(weatherArr);
-    } else {
-      response.status(500).send('Error from server');
-    }
+// -------------------------------------------
+// everything above this line is exactly what you need for an express (or close it)
+
+app.get('/', (request, response) => {
+  response.send('Hello, from the server! :)');
+});
+
+// class to get what we need from big data file
+class Forecast {
+  constructor(day) {
+    this.description = `Low: ${day.low_temp}, High: ${day.high_temp} with ${day.weather.description}`;
+    this.date = day.datetime;
   }
+}
+
+app.get('/weather', async (request, response) => {
+  let lat = request.query.lat;
+  let lon = request.query.lon;
+
+  let weatherResults = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
+  response.send(weatherResults.data.data.map((day) => new Forecast(day)));
+});
+
+class Movie {
+  constructor(movie) {
+    this.title = movie.original_title;
+    this.overview = movie.original_title;
+    this.average_votes = movie.vote_average;
+    this.total_votes = movie.vote_count;
+    this.img_url = movie.poster_path;
+    this.popularity = movie.popularity;
+    this.released_on = movie.release_date;
+  }
+}
+
+app.get('/movies', async (request, response) => {
+  //                        name in url 'city=<cityname>' ex: http://localhost:3001/movies?city=chicago
+  let query = request.query.city;
+
+  let movieResults = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${query}`);
+  response.send(movieResults.data.results.map((movie) => new Movie(movie)));
 });
 
 app.get('/*', (request, response) => {
   response.status(404).send('Path does not exists');
 });
-
-// app.get('/', (request, response) => {
-//   response.send('Hello, from the server!');
-// });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
